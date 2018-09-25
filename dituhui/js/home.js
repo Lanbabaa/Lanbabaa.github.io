24f889a805eaecf91597be58d29c0989ace17f5e
Lanbabaa <427742060@qq.com>
2018-09-25 08:04:11 +0800

    2

---
+++ dituhui/js/home.js
@@ -0,0 +1,116 @@
+layui.use('upload', function() {
+	var upload = layui.upload;
+	var uploadInst = upload.render({
+		elem: '.Up' //绑定元素
+			,
+		url: '/upload/' //上传接口
+			,
+		done: function(res) {
+			//上传完毕回调
+		},
+		error: function() {
+			//请求异常回调
+		}
+	});
+});
+layui.use('upload', function() {
+	var upload = layui.upload;
+	var uploadInst = upload.render({
+		elem: '.network' //绑定元素
+			,
+		url: '/upload/' //上传接口
+			,
+		done: function(res) {
+			//上传完毕回调
+		},
+		error: function() {
+			//请求异常回调
+		}
+	});
+});
+layui.use('carousel', function() {
+	var carousel = layui.carousel;
+	carousel.render({
+		elem: '#test1',
+		width: '100%',
+		height: '150px',
+		arrow: 'always',
+		indicator: "none"
+	});
+});
+
+function eye(e) {
+	var eye = e.className
+	if(eye == "fa fa-eye") {
+		e.className = "fa fa-eye-slash"
+	} else {
+		e.className = "fa fa-eye"
+	}
+}
+
+function addLayer() {
+	$(".layer").append("<li><i class='layui-icon layui-icon-location'></i><a>点图层</a><span>(500)</span><i class='fa fa-eye' id='eye2' onclick='eye(this)'></i></li>")
+}
+
+function TakeUp() {
+	$(".layui-nav-side,.content").fadeOut();
+	$(".main").css("left", "0px");
+	$(".main").css("width", "100%");
+	$("iframe").css("width", "100%");
+	$(".Show").fadeIn()
+}
+
+function Show() {
+	$(".layui-nav-side,.content").fadeIn();
+	$(".main").css("left", "500px");
+	$(".main").css("width", "1420px");
+	$("iframe").css("width", "1420px");
+	$(".Show").fadeOut()
+}
+$(".sort a").click(function() {
+	$(this).addClass("active");
+	$(this).siblings().removeClass("active")
+})
+$(".layui-card-body a").eq(1).click(function() {
+	$(".content .layui-card").css("display", "none");
+	$(".pointsLayer").css("display", "block");
+})
+$(".pointsLayerReturn a").click(function() {
+	$(".content .layui-card").css("display", "block");
+	$(".pointsLayer").css("display", "none");
+})
+$(".coordinate li").click(function(e) {
+	$(".coordinate").css("height", "370px");
+	$(".attribute").css("display", "block");
+	var title = $(e.target).text();
+	$(".title").text(title)
+
+})
+$(".attribute .layui-icon-close").click(function() {
+	$(".coordinate").css("height", "700px");
+	$(".attribute").css("display", "none");
+})
+$(".attribute .layui-icon-picture").click(function() {
+	$(".mask").css("display", "block");
+	$(".uppicture").fadeIn();
+})
+$(".pointsLayerBottom .import").click(function() {
+	$(".mask").css("display", "block");
+	$(".uploading").fadeIn();
+})
+$(".pointsLayerBottom .update").click(function() {
+	$(".mask").css("display", "block");
+	$(".updateData").fadeIn();
+})
+$(".uppicture .layui-icon-close").click(function() {
+	$(".mask").css("display", "none");
+	$(".uppicture").css("display", "none");
+})
+$(".uploading .layui-icon-close").click(function() {
+	$(".mask").css("display", "none");
+	$(".uploading").css("display", "none");
+})
+$(".updateData .layui-icon-close").click(function() {
+	$(".mask").css("display", "none");
+	$(".updateData").css("display", "none");
+})
