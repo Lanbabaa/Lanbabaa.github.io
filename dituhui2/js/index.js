b3390d46c290fa4179c3918a8ac944ccef0e063e
Lanbabaa <427742060@qq.com>
2018-10-16 14:07:00 +0800

    2

---
+++ dituhui2/js/index.js
@@ -0,0 +1,29 @@
+function Jump(e) {
+			$("iframe").attr("src", "" + e)
+		}
+		var n = 0;
+		$(".takeUp i").click(function() {
+			if(n == 0) {
+				$("a").addClass("visibilityHidden");
+				$("a i").addClass("visibilityVisible");
+				$(".teamHead").css("display", "none");
+				$(".layui-nav").css("width", "50px");
+				$(".takeUp i").attr("class", "layui-icon layui-icon-spread-left");
+				$(".takeUp i").attr("title", "点击展开");
+				$("iframe").addClass("open");
+				n++;
+				
+			} else {
+				$("a").removeClass("visibilityHidden");
+				$("a i").removeClass("visibilityVisible");
+				$(".teamHead").css("display", "block");
+				$(".layui-nav").css("width", "200px");
+				$(".takeUp i").attr("class", "layui-icon layui-icon-shrink-right");
+				$(".takeUp i").attr("title", "点击收起");
+				$("iframe").removeClass("open");
+				n--;
+			}
+		});
+		$(".MapList").click(function(){
+			
+		})
